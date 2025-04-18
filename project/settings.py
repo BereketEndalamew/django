85f030d989e8dc3b6f4587cb5c714f27cd7347d8
Mulusew Wube <mulusewwube0@gmail.com>
2025-04-17 18:48:37 -0700

    updte my mulusew first update

--- project/settings.py
+++ project/settings.py
@@ -78,16 +78,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'project',
-        'USER':'postgres',
-        'PASSWORD':'055872',
-        'HOST':'localhost',
-        'PORT':'5432'
-        # 'NAME': BASE_DIR / 'django.template',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
